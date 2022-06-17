8e05950d9e61fc00cf110c81075d636797440ce2
Mohamed Sabry <30502846+Mohamedfadah@users.noreply.github.com>
2022-06-17 14:21:27 +0200

    Edit routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Header from './containers/Header';
 import ProductListing from './containers/ProductListing';
 import ProductDetail from './containers/ProductDetail';
@@ -9,11 +9,11 @@ function App() {
     <div className="App">
       <Router>
         <Header />
-        <Switch>
-          <Route exact path='/' component={ProductListing} />
-          <Route path='/product/:id' component={ProductDetail} />
+        <Routes>
+          <Route exact path='/' element={<ProductListing />} />
+          <Route path='/product/:id' element={<ProductDetail />} />
           <Route>404 Not Found!</Route>
-        </Switch>
+        </Routes>
       </Router>
     </div>
   );
